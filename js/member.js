0155793f2d718f5bf8d58bb9d5f23e8b18ed2ec9
Rowdy <rowdyhowell@gmail.com>
2012-11-13 16:22:44 +0000

    Fixed location of index scripts. Got login to get appropriate response.

--- js/member.js
+++ js/member.js
@@ -6,7 +6,7 @@
 */
 
 //URL for member functions
-var memberurl='/picnit/api/member.php';
+var memberurl='api/member.php';
 
 //Request to be sent to the middleware
 var request;
@@ -33,10 +33,6 @@ function login() {
 	//Request to be sent to the middleware
 	request = new XMLHttpRequest();
 
-	//Create function that handles response
-	request.onreadystatechange = function () {
-		//Make sure request is ready, we only care about when done
-		if(request.readyState === 4) {
 			//Good data, proceed to login
 			if(request.status === 200) {
 				alert('200');
@@ -57,16 +53,33 @@ function login() {
 				alert('Unknown error: ' + request.status);
 			}
 		}
-	};
+	};*/
 
 	//Get and validate user inputs
-	request.open('POST', memberurl, true);
-		
+	request.open('POST', memberurl, false);
+
 	//Send request
 	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	var parms = 'username='+username+'&password='+password;
+	var parms = 'action=login&username='+username+'&password='+password;
 	request.send(parms);
-	
+
+	//Good data, proceed to login
+	if(request.status === 200) {
+		alert('200');
+	}
+	//Invalid username/password combo
+	else if(request.status === 204) {
+		alert('204');
+	}
+	//Our request messed up
+	else if(request.status === 400) {
+		alert('400');
+	}
+	//Something else went wrong
+	else {
+		alert('Unknown error: ' + request.status);
+	}
+
 	//Return true, meaning the request was sent, NOT that the login was successful
 	return false;
 }
