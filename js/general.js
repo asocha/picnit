7eb2b6ccfca09e3c81ce0e28709457952311cde1
Rowdy <rowdyhowell@gmail.com>
2012-11-13 22:03:02 +0000

    Added general.js, which will hold general javascript functions (cookies etc)

---
+++ js/general.js
@@ -0,0 +1,63 @@
+/*
+	general.js
+	Author: PhotoDolo
+
+	Contains functions that are to be used throughout the website
+	-Cookies
+	-Login check
+*/
+
+//Retrieve cookie that's stored
+function getCookie(NameOfCookie)
+{
+	//If we have cookies
+	if (document.cookie.length > 0) {
+		//Find it
+		begin = document.cookie.indexOf(NameOfCookie+"="); 
+		
+		//If found
+		if (begin != -1) {
+			//Get the ending
+			begin += NameOfCookie.length+1; 
+			end = document.cookie.indexOf(";", begin);
+			if (end == -1)
+				end = document.cookie.length;
+			
+			//Return the cookie value
+			return unescape(document.cookie.substring(begin, end));
+		} 
+	}
+
+	//No cookies or not found
+	return null; 
+}
+
+//Sets a cookies w/ expiration date
+function setCookie(NameOfCookie, value, expiredays) {
+	//Set the exipiration date
+	var ExpireDate = new Date ();
+	ExpireDate.setTime(ExpireDate.getTime() + (expiredays * 24 * 3600 * 1000));
+	
+	//Add the cookie
+	document.cookie = NameOfCookie + "=" + escape(value) + ((expiredays == null) ? "" : "; expires=" + ExpireDate.toGMTString());
+}
+
+//Deletes a cookie
+function deleteCookie(NameOfCookie) {
+	//If the cookie exists
+	if (getCookie(NameOfCookie)) {
+		//Set the expiration backwards in time, deleted immediately 
+		document.cookie = NameOfCookie + "=" + "; expires=Thu, 01-Jan-70 00:00:01 GMT";
+	}
+}
+
+function isLoggedIn() {
+	//Check to see if member_id cookie exists
+	if(getCookie('member_id')) {
+		//They are logged in
+		return true;
+	}
+
+	//No users logged in
+	return false;
+}
