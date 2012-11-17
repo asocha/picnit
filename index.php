1779a8c5712c5346a4b95941c10c43adc135e76d
Rowdy <rowdyhowell@gmail.com>
2012-11-17 19:34:56 +0000

    Made php/html/topbar.php contain the top bar for each page.

--- index.php
+++ index.php
@@ -30,34 +30,7 @@
 	</script>
 </head>
 <body>
-	<div id="menubar">
-		<?php
-			//Only show this section if user isn't logged in
-			if(!isLoggedIn()) {
-		?>
-		<form id="signupbut">
-			<span><input type="button" id="sign" value="Sign Up"/></span>
-		</form>
-	
-		<form id="signinform" action="index.php" onsubmit="return login();">
-			<span><label for="Username">username:</label><input type="text" id="Username"/></span>
-			<span><label for="Password">password:</label><input type="password" id="Password"/></span>
-			<span><input type="submit" id="signin" value="Sign In"/></span>
-		</form>
-		<form id="homebut" action="index.php">
-			<span><input type="submit" id="home" value="Home"/></span>
-		</form>
-		<?php
-			} //End if
-			else {
-		?>
-		<div id="userinfo">
-			<a href="profile.php?username=<?php echo $_COOKIE['username']; ?>"><span id="dispname"><?php echo $_COOKIE['username']; ?></a>
-		</div>
-		<?php
-			}
-		?>
-	</div>
+	<?php include 'php/html/topbar.php'; ?>
 
 	<img src="images/gui/largelogo.png" alt="picnit.net" height="150" id="logo">
 
