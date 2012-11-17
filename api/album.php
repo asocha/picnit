e6318e52f02b0929189540dc18988a15124be33b
Calvin Owens <jcalvinowens@gmail.com>
2012-11-17 16:30:35 -0600

    Add album API interface with dummy functions

---
+++ api/album.php
@@ -0,0 +1,35 @@
+<?php
+	//Include API Class
+	require_once("API.php");
+
+	class Album extends API {
+
+		public function __construct() {
+			//Parent Constructor
+			parent::__construct();
+
+			//Connect to the database
+			$this->link = $this->db_connect();
+		}
+
+		public function createAlbum()
+		{
+			$error = json_encode(array('status' => 'Failed', 'msg' => 'This is not implemented yet!'));
+			$this->response($error, 501);
+		}
+		
+		public function deleteAlbum()
+		{
+			$error = json_encode(array('status' => 'Failed', 'msg' => 'This is not implemented yet!'));
+			$this->response($error, 501);
+		}
+		
+		public function getImages()
+		{
+			$error = json_encode(array('status' => 'Failed', 'msg' => 'This is not implemented yet!'));
+			$this->response($error, 501);
+		}
+
+	$api = new Member;
+	$api->process();
+?>
