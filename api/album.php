589e52ce27e9440ad6b9b5352dc5b447b1edab09
Calvin Owens <jcalvinowens@gmail.com>
2012-11-18 22:43:01 -0600

    Add stuff to album.php

--- api/album.php
+++ api/album.php
@@ -14,8 +14,25 @@
 
 		public function createAlbum()
 		{
-			$error = json_encode(array('status' => 'Failed', 'msg' => 'This is not implemented yet!'));
-			$this->response($error, 501);
+			if ($this->memberid == -1) {
+				$error = json_encode(array('status' => 'Failed', 'msg' => 'You must authenticate to create albums'));
+				$this->response($error, 401);
+			}
+
+			$album_name = mysql_real_escape_string($_POST['name']);
+
+			if(isset($album_name)) {
+				$res = mysql_query("INSERT INTO albums (owner_id,date_created,name) VALUES ('$this->memberid',NOW(),'$album_name')", $this->link);
+				if(!$res) {
+					$error = json_encode(array('status' => 'Failed', 'msg' => 'Unknown error - try again'));
+					$this->response($error, 503);
+				}
+				$error = json_encode(array('status' => 'Failed', 'msg' => 'This is not implemented yet!'));
+				$this->response($error, 501);
+			}
+
+			$error = json_encode(array('status' => 'Failed', 'msg' => 'Your request is bad, and you should feel bad'));
+			$this->response($error, 400);
 		}
 		
 		public function deleteAlbum()
@@ -26,10 +43,37 @@
 		
 		public function getImages()
 		{
-			$error = json_encode(array('status' => 'Failed', 'msg' => 'This is not implemented yet!'));
-			$this->response($error, 501);
+			if ($this->memberid == -1) {
+				$error = json_encode(array('status' => 'Failed', 'msg' => 'You must authenticate to get images from albums, for now'));
+				$this->response($error, 401);
+			}
+
+			$albumid = mysql_real_escape_string($_POST['id']);
+
+			if(isset($albumid)) {
+				$res = mysql_query("SELECT image_id FROM images WHERE album_id='$id'");
+				if(!$res) {
+					$error = json_encode(array('status' => 'Failed', 'msg' => 'Unknown error - try again'));
+					$this->response($error, 503);
+				}
+				if(mysql_num_rows($res) == 0) {
+					$error = json_encode(array('status' => 'Success', 'msg' => 'Album is empty'));
+					$this->response($error, 204);
+				}
+				
+				while($row = mysql_fetch_array($res)) {
+					echo $row['image_id'];
+				}
+
+				$this->response('', 200);
+			}
+
+			$error = json_encode(array('status' => 'Failed', 'msg' => 'Your request is bad, and you should feel bad'));
+			$this->response($error, 400);
+		
 		}
 
 	$api = new Member;
 	$api->process();
+	}
 ?>
