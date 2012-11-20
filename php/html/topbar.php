71df887682b9fc063ddca46a83deeacfe084a42b
bdupree <btdupree@mac.com>
2012-11-19 22:41:40 -0600

    Update php/html/topbar.php

--- php/html/topbar.php
+++ php/html/topbar.php
@@ -2,28 +2,28 @@
 //Function that returns the top bar
 function menubar() {
 ?>
-	<div id="menubar">
+	<div id="menubar" class="panels">
 		<?php
 			//Only show this section if user isn't logged in
 			if(!isLoggedIn()) {
 		?>
 		<form id="signupbut">
-			<span><input type="button" id="sign" value="Sign Up"/></span>
+			<span><input type="button" id="sign" class="buttons" value="Sign Up"/></span>
 		</form>
 
 		<form id="signinform" action="index.php" onsubmit="return login();">
 			<span>
-				<label for="Username">username:</label>
-				<input type="text" id="Username" pattern="[\w]{3,15}" title="Must be between 3 and 15 letters, numbers, or underscores" required="required"/>
+				<label for="Username">username: </label>
+				<input type="text" id="Username" class="inputs" pattern="[\w]{3,15}" title="Must be between 3 and 15 letters, numbers, or underscores" required="required"/>
 			</span>
 			<span>
-				<label for="Password">password:</label>
-				<input type="password" id="Password" pattern=".{5,}" title="Must be at least 5 characters" required="required"/>
+				<label for="Password">password: </label>
+				<input type="password" id="Password" class="inputs" pattern=".{5,}" title="Must be at least 5 characters" required="required"/>
 			</span>
-			<span><input type="submit" id="signin" value="Sign In"/></span>
+			<span><input type="submit" id="signin" class="buttons" value="Sign In"/></span>
 		</form>
 		<form id="homebut" action="index.php">
-			<span><input type="submit" id="home" value="Home"/></span>
+			<span><input type="submit" id="home" class="buttons" value="Home"/></span>
 		</form>
 		<?php
 			} //End if
@@ -43,26 +43,26 @@ function menubar() {
 function signup() {
 	if(!isLoggedIn()) {
 ?>
-	<div id="signupbar">
+	<div id="signupbar" class="panels">
 		<form id="signupform" onsubmit="return createUser();">
 		<p><div>
 			<label for="Newusername">username: </label>
-			<input type="text" id="Newusername" pattern="[\w]{3,15}" title="Must be between 3 and 15 letters, numbers, or underscores" required="required"/>
+			<input type="text" id="Newusername" class="inputs" pattern="[\w]{3,15}" title="Must be between 3 and 15 letters, numbers, or underscores" required="required"/>
 		</div></p>
 		<p><div>
 			<label for="Newpassword">password: </label>
-			<input type="password" id="Newpassword" pattern=".{5,}" title="Must be at least 5 characters" required="required"/>
+			<input type="password" id="Newpassword" class="inputs" pattern=".{5,}" title="Must be at least 5 characters" required="required"/>
 		</div>
 		<div>
 			<label for="Confirmpassword">confirm password: </label>
-			<input type="password" id="Confirmpassword" required="required" oninput="validatePassword(document.getElementById('Newpassword'), this);"/>
+			<input type="password" id="Confirmpassword" class="inputs" required="required" oninput="validatePassword(document.getElementById('Newpassword'), this);"/>
 		</div></p>
 		<p><div>
 			<label for="name">email: </label>
-			<input type="email" id="email" required="required"/>
+			<input type="email" id="email" class="inputs" required="required"/>
 		</div></p>
-			<p><div><input type="submit" id="signup" value="sign up"/></div></p>
-			<p><div><input type="button" id="cancel" value="cancel"/></div><p>
+			<p><div><input type="submit" id="signup" class="buttons" value="sign up"/></div></p>
+			<p><div><input type="button" id="cancel" class="buttons" value="cancel"/></div><p>
 		</form>
 	</div>
 <?php
