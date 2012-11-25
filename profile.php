e7bcfbd5853f2ad496df8dfeb3e8421ab6e98683
Ben <btdupree@mac.com>
2012-11-25 01:39:29 +0000

    Updates to profile

--- profile.php
+++ profile.php
@@ -3,41 +3,25 @@
 	//Get general.php for DB calls
 	require_once('php/general.php');
 
-	//Make sure they send in a username
-	if(!isset($_GET['username']))
-		if(isLoggedIn())
-			$_GET['username'] = $_COOKIE['username'];
-		else
-			header("Location: index.php");
+	//Check log in
+	if(!isLoggedIn())
+		header('Location: index.php');
 	
 	//See if this user exists
-	$fields = array(
-		'action' => 'memberData',
-		'username' => urlencode($_COOKIE['username']),
-		'key' => urlencode($_COOKIE['key']),
-		'tusername' => urlencode($_GET['username'])
-	);
 
-	$res = picnitRequest('api/member.php', $fields);
-
-	//If does, get user info
-	if($res['status'] == 200) 
-		$profile = json_decode($res['result'], true);
-	else
-		header('Location: index.php');
 ?>
 <html>
-<title><?php echo  $profile['username']; ?>'s profile!</title>
+<title><?php echo  $_GET['username']; ?>'s profile!</title>
 <head>
 	<?php require_once('php/general.php'); ?>
 	<?php require_once('php/html/topbar.php'); ?>
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<link rel="stylesheet" href="css/flexslider.css" type="text/css">
 	<link href='http://fonts.googleapis.com/css?family=Concert+One' rel='stylesheet' type='text/css'>
-	<script type="text/javascript" src="js/libraries/jquery-1.8.2.min.js"></script>
-	<script type="text/javascript" src="js/libraries/jquery.flexslider-min.js"></script>
 	<script type="text/javascript" src="js/general.js"></script>
 	<script type="text/javascript" src="js/member.js"></script>
+	<script type="text/javascript" src="js/libraries/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/libraries/jquery.flexslider-min.js"></script>
 	<script>
 		window.onload = function() {
 			if(isLoggedIn()) {
@@ -61,7 +45,7 @@
 <body>
 	<?php menubar(); ?>
 	<div>
-		<h1><?php echo  $profile['username']; ?>'s profile</h1>
+		<h1><?php echo  $_GET['username']; ?>'s profile</h1>
 	</div>
 	<?php searchbar(); ?>
 	<div id="slideshow" class="flexslider">
@@ -78,6 +62,8 @@
 	  	</ul>
 	</div>
 	<div id="user-image-collection">
+		
+		<div id="collection" class="panels">
 		<div id="usermenu" class="panels">
 			<form id="userform">
 				<input id="albumsbut" class="buttons" type="button" value="Albums"/>
@@ -87,14 +73,18 @@
 				<input id="albumbut" class="buttons" type="button" value="Add Album"/>
 			</form>
 		</div>
-		<div id="collection" class="panels">
 			<div id="thumbnail-display">
+				<a href="images/AlArBdr.gif"><img src="images/AlArBdr.gif" alt="Pulpit rock" width="50" height="50"></a>
+				<a href="images/article-0-14C152E0000005DC-964_964x764.jpg"><img src="images/article-0-14C152E0000005DC-964_964x764.jpg" alt="Pulpit rock" width="50" height=50"></a>
+				<a href="images/FD_image.jpg"><img src="images/FD_image.jpg" alt="Pulpit rock" width="50" height="50"></a>
+				<a href="images/image16.gif"><img src="images/image16.gif" alt="Pulpit rock" width="50" height="50"></a>
+			<a href="images/pia12832-browse.jpg"><img src="images/pia12832-browse.jpg" alt="Pulpit rock" width="50" height="50"></a>
 			</div>
 		</div>
 	</div>
-	
+	<?php info(); ?>
 	<?php imageview(); ?>
 	<?php albumcreator(); ?>
-	<?php info(); ?>
+	
 </body>
 </html>
