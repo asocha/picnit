33c581ac14bec3b6ff43994dfa3785db7c277e37
AndrewS <sochaa@smu.edu>
2012-11-25 10:47:18 -0600

    added albumData

--- api/album.php
+++ api/album.php
@@ -69,6 +69,26 @@
 			$this->response(json_encode(array('status' => 'Success', 'list' => $tosend)), 200);	
 		}
 
+		public function albumData() {
+			$album_name = $this->load('album_name', false);
+                        $user_id = $this->load('user_id', false);
+
+                        if ($user_id == "" && $album_name == "") {
+                                $error = json_encode(array('status' => 'Failed', 'msg' => 'Missing data'));
+                                $this->response($error, 400);
+                        }
+                        else if($user_id != "")
+                                $res = mysql_query("SELECT album_id, owner_id, date_created, name, description FROM albums WHERE owner_id='$user_id'");
+                        else
+                                $res = mysql_query("SELECT album_id, owner_id, date_created, name, description FROM albums WHERE name='$album_name'");
+
+                        if(mysql_num_rows($res) < 1) {
+                                $error = json_encode(array('status' => 'Failed', 'msg' => 'Album does not exist'));
+                                $this->response($error, 409);
+                        }
+                        $array = mysql_fetch_array($res);
+                        $this->response(json_encode($array), 200);
+		}
 	}
 
 	$api = new Album;
