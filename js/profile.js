5c47d21b8c00e1a266235cd7a6c054a094360356
Rowdy <rowdyhowell@gmail.com>
2012-11-25 21:26:40 -0600

    Stuff and stuff, ready for Ben to work on.

--- js/profile.js
+++ js/profile.js
@@ -48,9 +48,9 @@
 		line+="<div class='imgdesc'>"+list[x]['description']+"</div>";
 		line+="<img src='data:" + list[x]['image_type'] + ";base64," + list[x]['image'] + "' alt='" + list[x]['name'] + "'/>";
 		line+="<div class='imgbuts'>";
-		line+="<input type='button' id='picdelbut"+list[x]['image_id']+"' class='buttons' value='delete'/>";
-		line+="<input type='button' id='picfavbut"+list[x]['image_id']+"' class='buttons' value='favorite'/>";
-		line+="<input type='button' id='pictagbut"+list[x]['image_id']+"' class='buttons' value='tag'/>";
+		line+="<input type='button' id='picdelbut"+list[x]['image_id']+"' class='buttons picdelbut' value='delete'/>";
+		line+="<input type='button' id='picfavbut"+list[x]['image_id']+"' class='buttons picfavbut' value='favorite'/>";
+		line+="<input type='button' id='pictagbut"+list[x]['image_id']+"' class='buttons pictagbut' value='tag'/>";
 		line+="</div>"
 		line+="</div>";
 	}
@@ -59,8 +59,10 @@
 	//Add event handlers
 	for(x in list) {
 		$("#picdelbut"+list[x]['image_id']).click(function() {
-			if(deleteImage(list[x]['image_id']))
-				$('#dispimage'+list[x]['image_id']).remove();
+			var id = $(this).attr('id').substring(9);
+			if(deleteImage(id)) {
+				$('#dispimage'+id).remove();
+			}
 		});
 
 	}
