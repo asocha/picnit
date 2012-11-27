51ef55227e10d285c8a99a1f7a65fc3f8aeb4828
Rowdy <rowdyhowell@gmail.com>
2012-11-26 18:04:53 -0600

    Added transition animations for switching panels.

--- js/profile.js
+++ js/profile.js
@@ -2,34 +2,12 @@
  *	profile.js
  *	Author: PhotoDolo
  *
- *	Creates the dynamic content of profile.php page
+ *	Creates the dynamic content
  */
 
- function createAlbumElements(uid) {
-	//Get the album information of this user
-	var list = getAlbums(uid);
+/***** album.php *****/
 
-	//Get display area
-	var disp = $("#thumbnail-display");
-
-	//Clear the current contents
-	disp.empty();
-	var line="<h2>albums</h2>";
-	for(x in list) {
-		line+="<div class='dispalbum'>";
-		line+="<a href='/picnit/album/"+list[x]['album_id']+"'>";
-		line+=list[x]['name'];
-		line+="</a>"
-		line+="<div class='albumdate'>"+list[x]['date_created']+"</div>";
-		line+="<p><div>";
-		line+=list[x]['description'];
-		line+="</div></p>";
-		line+="</div>";
-	}
-	disp.html(line);
- }
-
- function createAlbumImagesElements(album_id) {
+function createAlbumImagesElements(album_id) {
 	//Get the photos
 	var list = getImages(album_id);
 
@@ -51,7 +29,7 @@
 		line+="<input type='button' id='pictagbut"+list[x]['image_id']+"' class='buttons pictagbut' value='tag'/>";
 		line+="<input type='button' id='picfavbut"+list[x]['image_id']+"' class='buttons picfavbut' value='"+((list[x]['favorited'])? "unfavorite" : "favorite")+"'/>";
 		line+="<input type='button' id='picdelbut"+list[x]['image_id']+"' class='buttons picdelbut' value='delete'/>";
-		line+="</div>"
+		line+="</div>";
 		line+="</div>";
 	}
 	disp.html(line);
@@ -81,9 +59,9 @@
 			var id = $(this).attr('id').substring(9);
 		});
 	}
- }
+}
 
- function createFlexsliderElements(num,user_id) {
+function createFlexsliderElements(num,user_id) {
 	//Get the photos
 	var list = getLastImages(num,user_id);
 
@@ -97,25 +75,74 @@
 		line+="</li>";
 	}
 	disp.html(line);
- }
+}
 
- function createFavoritesElements() {
+/***** profile.php *****/
+
+function changePanel(elementFunction) {
+	$('#thumbnail-display').transition({ opacity: 0 }, 'fast', elementFunction);
+}
+
+function createAlbumElements(uid) {
+	//Get display area
+	var disp = $("#thumbnail-display");
+	
+	//Get the album information of this user
+	var list = getAlbums(uid);
+
+	//Build new html in meantime
+	var line="<h2>albums</h2>";
+	for(x in list) {
+		line+="<div class='dispalbum'>";
+		line+="<a href='/picnit/album/"+list[x]['album_id']+"'>";
+		line+=list[x]['name'];
+		line+="</a>"
+		line+="<div class='albumdate'>"+list[x]['date_created']+"</div>";
+		line+="<p><div>";
+		line+=list[x]['description'];
+		line+="</div></p>";
+		line+="</div>";
+	}
+	
+	//Clear the elements, should be invisible
+	disp.empty();
+
+	//Write new html
+	disp.html(line);
+	
+	//Make visible
+	disp.transition({opacity: 1}, 'fast');
+}
+
+function createFavoritesElements() {
+	//Get display area
+	var disp = $("#thumbnail-display");
+	
 	//Get the album information of this user
 	var list = getFavorites();
 
-	//Get display area
-	var disp = $("#thumbnail-display");
-
-	//Clear the current contents
-	disp.empty();
+	//Generate html
 	var line="<h2>favorites</h2>";
 	for(x in list) {
-		line += "<div class='dispfav'>";
-		line += list[x].toString();
-		line += "</div>";
+		line+="<div class='dispimage' id='dispimage"+list[x]['image_id']+"'>";
+		line+="<div class='imgdate'>"+list[x]['date_added']+"</div>";
+		line+="<div class='imgname'>"+list[x]['name']+"</div>";
+		line+="<div class='imgdesc'>"+list[x]['description']+"</div>";
+		line+="<img src='data:" + list[x]['image_type'] + ";base64," + list[x]['image'] + "' alt='" + list[x]['name'] + "'/>";
+		line+="<div class='imgbuts'>";
+		line+="<input type='button' id='picfavbut"+list[x]['image_id']+"' class='buttons picfavbut' value='"+((list[x]['favorited'])? "unfavorite" : "favorite")+"'/>";
+		line+="</div>";
+		line+="</div>";
 	}
+	//Clear the current contents
+	disp.empty();
+
+	//Write new html
 	disp.html(line);
- }
+
+	//Re display
+	disp.transition({opacity: 1}, 'fast');
+}
 
 function createTaggedElements() {
 	//Get the tagged elements
@@ -124,15 +151,20 @@ function createTaggedElements() {
 	//Get display area
 	var disp = $("#thumbnail-display");
 
-	//Clear the current contents
-	disp.empty();
 	var line="<h2>pictures of you</h2>";
 	for(x in list) {
 		line += "<div class='disptagged'>";
 		line += list[x].toString();
 		line += "</div>";
 	}
+	//Clear the current contents
+	disp.empty();
+
+	//Write new html
 	disp.html(line);
+
+	//Re display
+	disp.transition({opacity: 1}, 'fast');
 }
 
 function createFollowersElements() {
@@ -142,8 +174,6 @@ function createFollowersElements() {
 	//Get display area
 	var disp = $("#thumbnail-display");
 
-	//Clear the current contents
-	disp.empty();
 	var line="";
 	line+="<h2>followers</h2>";
 	for(x in list) {
@@ -154,7 +184,21 @@ function createFollowersElements() {
 		line+="<input type='button' id='folrembut"+list[x]['user_id']+"' class='buttons folrembut' value='remove'/>";
 		line += "</div>";
 	}
+	//Clear the current contents
+	disp.empty();
+
+	//Write new html
 	disp.html(line);
+
+	//Re display
+	disp.transition({opacity: 1}, 'fast');
+
+	//Set clicks
+	$('.folrembut').click(function() {
+		var id = $(this).attr('id').substring(9);
+		if(removeFollower(id))
+			$(this).parent().remove();
+	});
 }
 
 function createFolloweesElements() {
@@ -164,8 +208,6 @@ function createFolloweesElements() {
 	//Get display area
 	var disp = $("#thumbnail-display");
 
-	//Clear the current contents
-	disp.empty();
 	var line="";
 	line+="<h2>following</h2>";
 	for(x in list) {
@@ -176,7 +218,19 @@ function createFolloweesElements() {
 		line+="<input type='button' id='unfollbut"+list[x]['user_id']+"' class='buttons unfollbut' value='unfollow'/>";
 		line += "</div>";
 	}
+	//Clear the current contents
+	disp.empty();
+
+	//Write new html
 	disp.html(line);
+
+	//Re display
+	disp.transition({opacity: 1}, 'fast');
+
+	//Set clicks
+	$('.unfollbut').click(function() {
+		var id = $(this).attr('id').substring(9);
+	});
 }
 
 function createFollowReqElements() {
@@ -186,15 +240,34 @@ function createFollowReqElements() {
 	//Get display area
 	var disp = $("#thumbnail-display");
 
-	//Clear the current contents
-	disp.empty();
 	var line="<h2>follow requests</h2>";
 	for(x in list) {
 		line += "<div class='dispfollowreq'>";
 		line += "<a href='/picnit/profile/"+list[x]['username']+"'>";
 		line += "<span class='follower'>"+list[x]['username']+"</span>";
 		line += "</a>";
+		line += "<input type='button' id='accfolreq"+list[x]['user_id']+"' class='buttons accfolreq' value='accept'/>";
+		line += "<input type='button' id='decfolreq"+list[x]['user_id']+"' class='buttons decfolreq' value='decline'/>";
 		line += "</div>";
 	}
+	//Clear the current contents
+	disp.empty();
+
+	//Write new html
 	disp.html(line);
+
+	//Re display
+	disp.transition({opacity: 1}, 'fast');
+
+	//Set clicks
+	$('.accfolreq').click(function() {
+		var id = $(this).attr('id').substring(9);
+		if(follow(id))
+			$(this).parent().remove();
+	});
+	$('.decfolreq').click(function() {
+		var id = $(this).attr('id').substring(9);
+		if(refuseFollow(id))
+			$(this).parent().remove();
+	});
 }
